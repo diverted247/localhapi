c2050497ff5f3dd54628a7d6f8e1a985049a6352
Ted Patrick <ted@vestu.com>
2015-07-08 13:43:33 -0500

    first

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # localhapi
 A local hapijs server on any filesystem or port
+
+1. cd ~/
+2. git clone https://github.com/diverted247/localhapi.git
+3. cd localhapi
+4. npm install
+5. Add to ~./bash_profile
+
+`startLocalServer() {
+    if [ "$#" == 0 ]
+    then
+        local PORT=8888
+    else
+        local PORT=$1
+    fi
+    node ~/localhapi/server.js $PORT
+}
+ 
+alias _w=startLocalServer
+#_w      --> Opens HTTP server at path on port 8888
+#_w 9000 --> Opens HTTP server at path on port 9000
+`
+6. Open new console
+7. '_w 9000' or '_w' on any directory
+
+Edit to your needs!
+
+Ted :)
---
+++ server.js
@@ -0,0 +1,30 @@
+var Hapi = require('hapi');
+var exec = require('child_process').exec;
+
+var port = process.argv[ 2 ]
+if( port == undefined ){
+    port = 8888;
+}
+var server = new Hapi.Server();
+server.connection({ port: port });
+
+var root = process.cwd() + '/';
+
+server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+        directory: {
+            path: root,
+            listing:true,
+            index:true
+        }
+    }
+});
+
+server.start( function(){
+    console.log( 'Server running at:' , server.info.uri );
+    console.log( 'Server root:' , root );
+    exec( "open http://localhost:" + port  , function(error, stdout, stderr) {
+    });
+});
